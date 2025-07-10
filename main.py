43302551217739bebc3550ec1655d75591ee3683
jojojoester <aashishrokka3@gmail.com>
2025-07-10 23:30:43 +0545

    added http operations

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title =  "Welcome to NoteAPI", 
+    description = "A RESTful API (or web API) that lets users create, read, update, and delete (CRUD) notes through HTTP requests."
+)
 
 @app.get("/")
 def read_notes():
     return{"Message": "Read Notes"}
 
 @app.post("/")
-def read_notes():
+def add_notes():
     return{"Message": "Add Notes"}
 
 @app.put("/")
-def read_notes():
+def update_notes():
     return{"Message": "Update Notes"}
 
 @app.delete("/")
-def read_notes():
+def delete_notes():
     return{"Message": "Delete Notes"}
